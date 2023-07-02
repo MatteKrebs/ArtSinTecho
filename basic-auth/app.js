75e426263beaeaa4afdea1d3e274fc1762c40313
Vialoha <vi62469@gmail.com>
2023-07-02 10:32:19 +0100

    initial user setup

--- basic-auth/app.js
+++ basic-auth/app.js
@@ -1,33 +1,26 @@
-
 // ℹ️ Gets access to environment variables/settings
 // https://www.npmjs.com/package/dotenv
 require("dotenv").config();
 
 // ℹ️ Connects to the database
 require("./db");
-const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://127.0.0.1:27017/ArtSinTecho')
-    .then(() => console.log('Database good.'))
-    .catch(err => console.log(err));
-
-
-
+// Handles http requests (express is node js framework)
+// https://www.npmjs.com/package/express
 const express = require("express");
-const app = express();
 
+// Handles the handlebars
+// https://www.npmjs.com/package/hbs
 const hbs = require("hbs");
-app.set('view engine', 'hbs');
-app.set('views', __dirname+'/views');
 
-app.use(express.static('public'));
+const app = express();
 
 // ℹ️ This function is getting exported from the config folder. It runs most pieces of middleware
 require("./config")(app);
 
 // default value for title local
 const capitalize = require("./utils/capitalize");
-const projectName = "ArtSinTecho";
+const projectName = "basic-auth";
 
 app.locals.appTitle = `${capitalize(projectName)} created with IronLauncher`;
 
@@ -35,20 +28,7 @@ app.locals.appTitle = `${capitalize(projectName)} created with IronLauncher`;
 const indexRoutes = require("./routes/index.routes");
 app.use("/", indexRoutes);
 
-
-// Import the route files
-const artistsRoutes = require('./routes/artist.routes');
-const artworkRoutes = require('./routes/artwork.routes');
-const authRoutes = require("./routes/auth.routes");
-
-// Link the route files to the main router
-app.use('/', artistsRoutes);
-app.use('/', artworkRoutes);
-app.use('/auth', authRoutes)
-
-
 // ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
 require("./error-handling")(app);
-
 
 module.exports = app;
--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,8 +1,52 @@
+const bcrypt = require ('bcrypt');
+const saltRounds = 10;
+
+const User = require('../models/User.model')
+
 const express = require('express');
 const router = express.Router();
 
+/* GET home page */
+router.get("/signup", (req, res, next) => {
+    res.render('auth/signup')
+  });
 
 
+/* GET home page */
+router.post("/signup", (req, res, next) => {
+  
+    const {username, password, city, isAdmin, userPicture} = req.body;
+
+    bcrypt
+      .genSalt(saltRounds)
+      .then(salt => bcrypt.hash(password, salt))
+      .then(hashedPassword => {
+        return User.create({
+          username,
+          city,
+          isAdmin,
+          userPicture,
+          password: hashedPassword
+        });
+      })
+      .then(userFromDB => {
+        console.log('New user added', userFromDB);
+        res.redirect(`/auth/profile/${userFromDB.username}`)
+      })
+      .catch(error => next(error));
+  });
+
+
+
+/* GET home page */
+router.get("/profile/:username", (req, res, next) => {
+    User.findOne({username: req.params.username})
+      .then(foundUser => {
+        console.log('foundUser', foundUser)
+        res.render('auth/profile', foundUser)
+      })
+      .catch(err => console.log(err))
+  });
 
 
 module.exports = router;
